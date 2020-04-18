db07bf5153eaaf3ad3acb9e27e76641a65772792
marpiotoun <plus470@naver.com>
2020-04-18 18:55:00 +0900

    Update day5.py
    Nomad code challenge

--- day5.py
+++ day5.py
@@ -1,23 +1,32 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
 
+os.system("clear")
+url = "https://www.iban.com/currency-codes"
+
+
 def get_countries_currency():
-    tr = BeautifulSoup(requests.get("https://www.iban.com/currency-codes").text, 'html.parser').\
+    tr = BeautifulSoup(requests.get("https://www.iban.com/currency-codes").text, 'html.parser'). \
         find("table", class_="table table-bordered downloads tablesorter").find_all("tr")
     country_dict = {}
+    non_counter = 0
     for idx, element in enumerate(tr):
+
         row = []
         for i in element:
             line = i.string
             if line != '\n':
                 row.append(i.string)
-        country_dict.update({idx: tuple(row)})
+        if row[2] is None:
+            non_counter += 1
+            continue
+        country_dict.update({idx - non_counter: tuple(row)})
     return country_dict
 
 
 def get_input_and_print_code(country_dict):
-
     for i in range(1, len(country_dict)):
         print(f"# {i} {country_dict[i][0]}")
 
@@ -26,7 +35,7 @@ def get_input_and_print_code(country_dict):
     while flag:
         try:
             number = int(input("#: "))
-            if not 0 < number < 269:
+            if not 0 < number < len(country_dict):
                 print("Choose a number from the list.")
                 continue
             flag = False
@@ -34,7 +43,3 @@ def get_input_and_print_code(country_dict):
             print("That wasn't a number.")
     print(f"You choose {country_dict[number][0]}")
     print(f"The currency code is {country_dict[number][2]}")
-
-
-countries_dict = get_countries_currency()
-get_input_and_print_code(countries_dict)
